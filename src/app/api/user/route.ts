4c0869940e5039155a645ea67b8abd9680de253f
“Kapil <doctypecode@gmail.com>
2023-07-02 17:09:54 +0530

    1) Setup Prisma 2) Created CRUD for User 3) Initial Setup

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,59 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import {
+  createUser,
+  deleteUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+} from "../../../../prisma/user/user";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const email = req.nextUrl.searchParams.get("email");
+  if (email) {
+    const user = await getUser(email);
+    if (!user) {
+      return NextResponse.json(
+        { data: "No user found with this emailId" },
+        { status: 200 }
+      );
+    }
+    return NextResponse.json({ data: user }, { status: 200 });
+  } else {
+    // Otherwise, fetch all users
+    const users = await getAllUsers();
+    return NextResponse.json({ data: users }, { status: 200 });
+  }
+}
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  // Create a new user
+  if (body) {
+    try {
+      const user = await createUser(body);
+      return NextResponse.json({ data: user }, { status: 200 });
+    } catch (error: any) {
+      return NextResponse.json({ error }, { status: 500 });
+    }
+  }
+  return NextResponse.json(
+    { data: "Please add the required fields" },
+    { status: 500 }
+  );
+}
+
+export async function PUT(req: NextRequest) {
+  const body = await req.json();
+
+  const { email, ...updateData } = body;
+  const user = await updateUser(email, updateData);
+  return NextResponse.json({ data: user }, { status: 200 });
+}
+
+export async function DELETE(req: NextRequest) {
+  const body = await req.json();
+
+  const user = await deleteUser(body.email);
+  return NextResponse.json({ data: user }, { status: 200 });
+}
